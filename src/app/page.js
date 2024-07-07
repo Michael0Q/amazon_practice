e1e2927c73c6a3b746061a3467dda88721ec6354
まいける <167097617+Michael0Q@users.noreply.github.com>
2024-07-07 14:11:53 +0900

    ベース作成

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,28 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main style={{textAlign: "center"}}>
-      <div>
+        <div style={{display : "flex", justifyContent: "space-between"}}>
+          <Link href='/home'>
+            ホーム
+          </Link>
+          <Link href='/store'>
+            ストア
+          </Link>
+          <Link href='/live_tv'>
+            ライブTV
+          </Link>
+          <Link href='/category'>
+            カテゴリー
+          </Link>
+          <Link href='/myitem'>
+            マイアイテム
+          </Link>
+        </div>
         <h1 style={{color : 'white', marginTop: '30%', fontSize: '50px' }}>ペオスアイティープレーン</h1>
-      </div>
     </main>
   );
 }
