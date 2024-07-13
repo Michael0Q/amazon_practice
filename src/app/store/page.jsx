66418663eabd70bbc041d1184743f99dee60eabd
ito_ru <zgmfx10aruka@gmail.com>
2024-07-13 15:49:19 +0900

    pushtest

---
+++ src/app/store/page.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/header/Header'
+
+const page = () => {
+    return(
+        <div>
+            <Header></Header>
+            <h1>ストア</h1>
+        </div>
+    )
+}
+
+export default page
